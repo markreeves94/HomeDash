36ab6a3cbc910121b046885b0b943db041a5caf1
markreeves94 <markreeves94@hotmail.co.uk>
2026-06-15 11:06:55 +0100

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,6 +1,6 @@
 console.log("calendar.js loaded successfully");
 
-const CALENDAR_URL = "const CALENDAR_URL = "https://eur01.safelinks.protection.outlook.com/?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fxxxxxxxx%2Fpublic%2Fbasic.ics&data=05%7C02%7Cmark.reeves%40dssmith.com%7C1216c0ddfa704c3ee16508decac4a57c%7C423430e8247c44d1976722723b7d4cb2%7C0%7C0%7C639171143099814255%7CUnknown%7CTWFpbGZsb3d8eyJFbXB0eU1hcGkiOnRydWUsIlYiOiIwLjAuMDAwMCIsIlAiOiJXaW4zMiIsIkFOIjoiTWFpbCIsIldUIjoyfQ%3D%3D%7C0%7C%7C%7C&sdata=gE6ztf3Q2Ggys7FNJ7iniocPQNx%2BSYFTykQl99hFgcc%3D&reserved=0"";
+const CALENDAR_URL = "const CALENDAR_URL = https://eur01.safelinks.protection.outlook.com/?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fxxxxxxxx%2Fpublic%2Fbasic.ics&data=05%7C02%7Cmark.reeves%40dssmith.com%7C1216c0ddfa704c3ee16508decac4a57c%7C423430e8247c44d1976722723b7d4cb2%7C0%7C0%7C639171143099814255%7CUnknown%7CTWFpbGZsb3d8eyJFbXB0eU1hcGkiOnRydWUsIlYiOiIwLjAuMDAwMCIsIlAiOiJXaW4zMiIsIkFOIjoiTWFpbCIsIldUIjoyfQ%3D%3D%7C0%7C%7C%7C&sdata=gE6ztf3Q2Ggys7FNJ7iniocPQNx%2BSYFTykQl99hFgcc%3D&reserved=0";
 
 async function loadCalendar() {
     console.log("Loading calendar...");
