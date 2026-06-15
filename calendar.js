708af220a8d32f6bec306cb3597717fe34c7ba3b
markreeves94 <markreeves94@hotmail.co.uk>
2026-06-15 11:19:03 +0100

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,6 +1,6 @@
 console.log("calendar.js loaded successfully");
 
-const CALENDAR_URL = "const CALENDAR_URL = "https://calendar.google.com/calendar/ical/ampureeves%40gmail.com/public/basic.ics";
+const CALENDAR_URL = "https://calendar.google.com/calendar/ical/ampureeves%40gmail.com/public/basic.ics";
 
 async function loadCalendar() {
     console.log("Loading calendar...");
